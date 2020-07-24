64c25d5e56405d209a8ad298e7cbfbaf4febbd0b
Kevin Rosario <spm.kerosario@gmail.com>
2020-07-24 11:54:38 +0800

    Add Has role trait on User model

--- app/User.php
+++ app/User.php
@@ -3,15 +3,15 @@
 namespace App;
 
 use App\Notifications\PasswordReset;
-use App\Notifications\ResetPasswordNotificaiton;
 use App\Notifications\ResetPasswordNotification;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use SoftDeletes, Notifiable;
+    use HasRoles,SoftDeletes, Notifiable;
 
     /**
      * The attributes that are mass assignable.
