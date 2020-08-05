661762313892dc56d505ddb7a9c5048e68db42c0
Kevin Rosario <spm.kerosario@gmail.com>
2020-08-05 09:23:10 +0800

    Move Web Layout view and removed unused views

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
-//Auth::routes();
-
-//Route::get('/home', 'HomeController@index')->name('home');
-
 Route::get('/{vue?}', function () {
-    return view('layouts.master');
-})->where('vue', '[\/\w\.-]*')->name('master');
+    return view('layouts.app');
+})->where('vue', '[\/\w\.-]*')->name('app');
 
